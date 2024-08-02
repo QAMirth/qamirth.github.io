26650c553a89c4f865eb9c708173d7836d26045c
QAMirth <katzeniy008@gmail.com>
2024-08-02 23:43:32 +0300

    Update menu.js

--- csjs/menu.js
+++ csjs/menu.js
@@ -9,31 +9,44 @@ document.write(`
                     <li class="uWithSubmenu"><a class="m1" href="/1/1.html" ><span>Static testing</span></a>
                         <ul>
                             <li><a class="m11" href="/1/1.html"><span>Static Review</span></a></li>
-                             <li><a class="m12" href="/1/11.html"><span>Static Analysis</span></a></li> 
-                             <li><a class="m12" href="/1/12.html">Requirements Management Tools List</a></li>
+                             <li><a class="m111" href="/1/11.html"><span>Static Analysis</span></a></li> 
                     </ul>
                     </li>
-                    <li class="uWithSubmenu"><a class="m2" href="/2/2.html" ><span>Explore</span></a>
+                     <li class="uWithSubmenu"><a class="m3" href="/3/3.html" ><span>Design Techniques</span></a>
                         <ul>
-                            <li><a class="m21" href="/2/2.html" ><span>Exploratory testing</span></a></li>
+                            <li><a class="m33" href="/3/3.html" ><span>Black-box</span></a></li>
+                             <li><a class="m331" href="/3/31.html" ><span>White-box</span></a></li>
                         </ul>
                     </li>
-                    <li class="uWithSubmenu"><a class="m3" href="/3/3.html" ><span>Design Techniques</span></a>
+                    
+                    <li class="uWithSubmenu"><a class="m2" href="/2/2.html" ><span>Level</span></a>
                         <ul>
-                            <li><a class="m31" href="/3/3.html" ><span>Black-box</span></a></li>
-                             <li><a class="m31" href="/3/31.html" ><span>White-box</span></a></li>
+                            <li><a class="m22" href="/2/2.html" ><span>Unit</span></a></li>
+                            <li><a class="m221" href="/2/21.html" ><span>Integration</span></a></li>
+                            <li><a class="m222" href="/2/22.html" ><span>System</span></a></li>
                         </ul>
                     </li>
-                    <li class="uWithSubmenu"><a class="m4" href="/4/4.html" ><span>Non-functional</span></a>
+                    
+                    <li class="uWithSubmenu"><a class="m4" href="/4/4.html" ><span>Change-related</span></a>
                         <ul>
-                            <li><a class="m41" href="/4/41.html" ><span>Yooo</span></a></li>
+                        <li><a class="m44" href="/4/4.html" ><span>Smoke</span></a></li>
+                            <li><a class="m441" href="/4/41.html" ><span>Sanity</span></a></li>
+                            <li><a class="m442" href="/4/42.html" ><span>Regression</span></a></li>
                         </ul>
                     </li>
-                    <li class="uWithSubmenu"><a class="m5" href="/5/5.html" ><span>Other</span></a>
+                    <li class="uWithSubmenu"><a class="m5" href="/5/5.html" ><span>Non-functional</span></a>
                         <ul>
-                            <li><a class="m51" href="/5/51.html" ><span>Zooo</span></a></li>
+                            <li><a class="m55" href="/5/5.html" ><span>Performance</span></a></li>
+                              <li><a class="m551" href="/5/51.html" ><span>Ui/UX</span></a></li>
+                               <li><a class="m552" href="/5/52.html" ><span>Security</span></a></li>
                         </ul>
                     </li>
+                    <li><a class="m81" href="/8/1.html" ><span>Automated</span></a></li>
+                    <li><a class="m82" href="/8/2.html" ><span>Mobile testing</span></a></li>
+                    <li><a class="m83" href="/8/3.html" ><span>Test case based</span></a></li>
+                    <li><a class="m84" href="/8/4.html" ><span>Exploratory/Ad hoc</span></a></li>
+                    <li><a class="m85" href="/1/12.html" ><span>Management Tools List</span></a></li>
+                   
                   </ul>
             </div>
             <!-- </bc> -->
