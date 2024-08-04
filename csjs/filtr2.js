15b928c2eed889d117591d4b092bbf0ebd205e75
QAMirth <katzeniy008@gmail.com>
2024-08-05 01:14:07 +0300

    Create filtr2.js

---
+++ csjs/filtr2.js
@@ -0,0 +1,14 @@
+document.write(`
+<div id="filters">
+    <select id="sort-filter" onchange="sortMaterials()">
+        <option value="title-asc">Titleа (A)</option>
+        <option value="title-desc">Title (D)</option>
+    </select>
+    <select id="format-filter" onchange="filterMaterials()">
+        <option value="">All Formats</option>
+    </select>
+    <select id="freepaid-filter" onchange="filterMaterials()">
+        <option value="">All Free/Paid</option>
+    </select>
+</div>
+`);
