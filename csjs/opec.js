183cbd4872eff8d9acb29c80aa1549b5909082ff
QAMirth <katzeniy008@gmail.com>
2024-08-05 20:36:27 +0300

    Create opec.js

---
+++ csjs/opec.js
@@ -0,0 +1,26 @@
+      document.querySelectorAll('.button-link2').forEach(button => {
+            button.addEventListener('click', function() {
+                const desc = this.parentElement;
+                desc.classList.add('expanded');
+                this.style.display = 'none';
+                const buttonLink2 = desc.parentElement.querySelector('.button-link3');
+                buttonLink2.textContent = 'Close';
+                buttonLink2.style.display = 'inline';
+            });
+        });
+
+        document.querySelectorAll('.button-link3').forEach(button => {
+            button.addEventListener('click', function() {
+                const desc = this.closest('.info-card').querySelector('.info-card-desc');
+                const isExpanded = desc.classList.toggle('expanded');
+                const buttonLink = desc.querySelector('.button-link2');
+                if (isExpanded) {
+                    buttonLink.style.display = 'none';
+                    this.textContent = 'Close';
+                } else {
+                    buttonLink.style.display = 'inline';
+                    this.textContent = 'More info';
+                    this.style.display = 'none';
+                }
+            });
+        });
