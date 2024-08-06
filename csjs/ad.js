727541f0be266004c7bec10159465abe388c8e61
QAMirth <katzeniy008@gmail.com>
2024-08-06 18:03:49 +0300

    Update ad.js

--- csjs/ad.js
+++ csjs/ad.js
@@ -1,19 +1,9 @@
 document.write(`
- <style>
-        .toolbar {
-            border: 1px solid #ccc;
-            padding: 5px;
-        }
-
-        .toolbar button {
-            margin-right: 5px;
-        }
-
-        #description {
-            border: 1px solid #ccc;
-            padding: 10px;
-            min-height: 200px;
-            overflow: auto;
+ <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+    <style>
+        #description-container {
+            height: 300px;
         }
     </style>
 <button id="add-material-btn">+</button>
@@ -22,18 +12,7 @@ document.write(`
             <label for="title">Title:</label>
             <input type="text" id="title" name="title" required><br>
             <label for="description">Description:</label>
-            <div class="toolbar">
-                <button type="button" onclick="formatText('bold')"><b>B</b></button>
-                <button type="button" onclick="formatText('italic')"><i>I</i></button>
-                <button type="button" onclick="formatText('underline')"><u>U</u></button>
-                <button type="button" onclick="formatText('strikeThrough')"><s>S</s></button>
-                <button type="button" onclick="formatBlock('H1')">H1</button>
-                <button type="button" onclick="formatBlock('H2')">H2</button>
-                <button type="button" onclick="formatBlock('H3')">H3</button>
-                <button type="button" onclick="createLink()">Link</button>
-                <button type="button" onclick="insertImage()">Image</button>
-            </div>
-            <div id="description" contenteditable="true"></div><br>
+            <div id="description-container"></div><br>
             <label for="format">Format:</label>
             <input type="text" id="format" name="format" required><br>
             <label for="freepaid">Free/Paid:</label>
@@ -51,52 +30,20 @@ document.write(`
         </form>
         <button class="close">Close</button>
     </div>
-    
-    <script>
+     <script>
         document.addEventListener('DOMContentLoaded', function() {
-            const popup = document.getElementById('popup-form');
-            const addBtn = document.getElementById('add-material-btn');
-            const closeBtn = document.querySelector('.close');
+            var quill = new Quill('#description-container', {
+                theme: 'snow'
+            });
 
-            addBtn.onclick = function() {
-                popup.style.display = "block";
-            }
-
-            closeBtn.onclick = function() {
-                popup.style.display = "none";
-            }
-
-            window.onclick = function(event) {
-                if (event.target == popup) {
-                    popup.style.display = "none";
-                }
-            }
-
+            // Handle form submission to include Quill data
             document.getElementById('material-form').onsubmit = function() {
-                const descriptionInput = document.createElement('input');
+                var descriptionInput = document.createElement('input');
                 descriptionInput.setAttribute('type', 'hidden');
                 descriptionInput.setAttribute('name', 'description');
-                descriptionInput.value = document.getElementById('description').innerHTML;
+                descriptionInput.value = quill.root.innerHTML;
                 this.appendChild(descriptionInput);
             };
         });
-
-        function formatText(command) {
-            document.execCommand(command, false, null);
-        }
-
-        function formatBlock(tag) {
-            document.execCommand('formatBlock', false, tag);
-        }
-
-        function createLink() {
-            const url = prompt("Enter the link URL:", "http://");
-            document.execCommand('createLink', false, url);
-        }
-
-        function insertImage() {
-            const url = prompt("Enter the image URL:", "http://");
-            document.execCommand('insertImage', false, url);
-        }
     </script>
 `);
