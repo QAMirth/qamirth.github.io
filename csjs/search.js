15b17b0c3580a27165f0d37641507331c9dc74ec
QAMirth <katzeniy008@gmail.com>
2024-08-07 21:20:37 +0300

    Create search.js

---
+++ csjs/search.js
@@ -0,0 +1,17 @@
+document.write(`
+<div class="sidebar-block-content sidebar-search-content">
+							
+		<div class="searchForm">
+			<form onsubmit="this.sfSbm.disabled=true" method="get" style="margin:0" action="/search/">
+				<div align="center" class="schQuery">
+					<input type="text" name="q" maxlength="30" size="20" class="queryField">
+				</div>
+				<div align="center" class="schBtn">
+					<input type="submit" class="searchSbmFl" name="sfSbm" value="Search">
+				</div>
+				<input type="hidden" name="t" value="0">
+			</form>
+		</div>
+							<svg class="svg-icon search-form-icon"><use xlink:href="/.s/t/2301/icons.svg#search_icon"></use></svg>
+						</div>
+  `);
