57dbc4a75bf396ccd431025b3bbccb0b92cf4b22
QAMirth <katzeniy008@gmail.com>
2024-08-07 21:56:07 +0300

    Create filter2-filters.js

---
+++ csjs/filter2-filters.js
@@ -0,0 +1,10 @@
+document.write(`
+<div id="filters">
+    <select id="format-filter" onchange="filterMaterials()">
+        <option value="">- All Formats</option>
+    </select>
+    <select id="freepaid-filter" onchange="filterMaterials()">
+        <option value="">- All Free/Paid</option>
+    </select>
+</div>
+`);
