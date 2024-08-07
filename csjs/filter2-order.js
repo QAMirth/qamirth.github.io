a2b448a839a03dc62ae6eaa6c7fe6ab5ea0dbb0b
QAMirth <katzeniy008@gmail.com>
2024-08-07 21:55:13 +0300

    Create filter2-order.js

---
+++ csjs/filter2-order.js
@@ -0,0 +1,8 @@
+document.write(`
+<div id="filters-order">
+    <select id="sort-filter" onchange="sortMaterials()">
+        <option value="title-asc">Titleа (A)</option>
+        <option value="title-desc">Title (D)</option>
+    </select>
+</div>
+`);
