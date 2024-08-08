9a84142e20056f16b3f86e09b8515891b5e09e76
QAMirth <katzeniy008@gmail.com>
2024-08-08 04:43:51 +0300

    Update filtr2-filters.js

--- csjs/filtr2-filters.js
+++ csjs/filtr2-filters.js
@@ -1,10 +1,10 @@
 document.write(`
 <div id="filters-filters">
     <select id="format-filter" onchange="filterMaterials()">
-        <option value="">- All Formats</option>
+        <option value="">All Formats</option>
     </select>
     <select id="freepaid-filter" onchange="filterMaterials()">
-        <option value="">- All Free/Paid</option>
+        <option value="">All Free/Paid</option>
     </select>
 </div>
 `);
