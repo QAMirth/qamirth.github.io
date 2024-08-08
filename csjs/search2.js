29129b4864030869955abcc5de7aab223bd16f82
QAMirth <katzeniy008@gmail.com>
2024-08-09 01:33:19 +0300

    Update search2.js

--- csjs/search2.js
+++ csjs/search2.js
@@ -20,7 +20,7 @@ name="q"
 maxlength="30"
 size="20"
 autocomplete="off">
-<button class="header-search-button" type="submit">Sear</button>
+<button class="header-search-button" type="submit"></button>
 </form>
 </div>
 </div>
