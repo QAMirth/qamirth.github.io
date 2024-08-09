898862cf7724be4752ffe315e6e404650cce7428
QAMirth <katzeniy008@gmail.com>
2024-08-09 16:58:39 +0300

    Update login-signup-menu.js

--- csjs/login-signup-menu.js
+++ csjs/login-signup-menu.js
@@ -1,5 +1,4 @@
 // login-signup-menu.js
 document.write(`
-<a title="Log In" href="javascript:;" rel="nofollow" onclick="loginPopupForm(); return false;"><!--<s3087>-->Log In<!--</s>--></a> | 
-<a title="Sign Up" href="/register"><!--<s3089>-->Sign Up<!--</s>--></a>
+<a title="Add tool" class="addd" href="/99/add.html">Add tool</a>
 `);
