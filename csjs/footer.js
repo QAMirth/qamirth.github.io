e7d15490674d5a0b273a4d6840981e26f47cdb17
QAMirth <katzeniy008@gmail.com>
2024-08-09 17:37:25 +0300

    Update footer.js

--- csjs/footer.js
+++ csjs/footer.js
@@ -2,17 +2,17 @@
       <div class="container footer-container">
         <div class="footer-left">
           <div class="copy">
-            <!-- <copy> -->
+           
             Copyright <span class="kz">
-              <a href="../99/feedback.html" target="_blank" rel="nofollow">@Katzeniy</a>
+              <a href="../99/contacts.html">@Katzeniy</a>
             </span> © 2024 from the Nativity of Christ
-            <!-- </copy> -->
+           
           </div>
           <div class="copy2"></div>
         </div>
         <div class="footer-right">
           <span class="katzeniy">
-            <a href="../99/feedback.html" target="_blank">Contact the author</a>
+            <a href="../99/contacts.html">Contact the author</a>
           </span>
           <span class="adt2">
             <a href="../99/add.html" target="_blank">Add a tool</a>
