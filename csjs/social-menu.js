5cf08c18830f9e4053f445ba0d3984e101ad4f66
QAMirth <katzeniy008@gmail.com>
2024-08-30 19:37:19 +0300

    Update social-menu.js

--- csjs/social-menu.js
+++ csjs/social-menu.js
@@ -4,32 +4,16 @@ document.write(`
     <p class="menu-social-title">Find us on social media</p>
     <ul class="social-list menu-social-list">
         <li>
-            <a class="social-list-link vk-link" href="#" target="_blank">
-                <svg class="social-list-icon">
-                    <use xlink:href="/icons.svg#vk_icon"></use>
-                </svg>
-            </a>
+           
         </li>
         <li>
-            <a class="social-list-link fb-link" href="#" target="_blank">
-                <svg class="social-list-icon">
-                    <use xlink:href="/icons.svg#fb_icon"></use>
-                </svg>
-            </a>
+           
         </li>
         <li>
-            <a class="social-list-link ok-link" href="#" target="_blank">
-                <svg class="social-list-icon">
-                    <use xlink:href="/icons.svg#ok_icon"></use>
-                </svg>
-            </a>
+            
         </li>
         <li>
-            <a class="social-list-link tg-link" href="#" target="_blank">
-                <svg class="social-list-icon">
-                    <use xlink:href="/icons.svg#tg_icon"></use>
-                </svg>
-            </a>
+           
         </li>
     </ul>
 </div>
