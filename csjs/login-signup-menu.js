5d9377efb9a64d324b8e4e00c37c1803cc52c62d
QAMirth <katzeniy008@gmail.com>
2024-08-31 18:18:25 +0300

    Update login-signup-menu.js

--- csjs/login-signup-menu.js
+++ csjs/login-signup-menu.js
@@ -1,4 +1,13 @@
-// login-signup-menu.js
+
 document.write(`
+<div id="textSizeFilter" style="position: relative;">
+    <button id="eyeButton" style="font-size: 24px;">👁</button>
+    <ul id="sizeOptions" style="display: none; list-style-type: none; padding: 0; margin: 0; position: absolute; background-color: white; border: 1px solid #ccc;">
+        <li><button data-size="small">Small</button></li>
+        <li><button data-size="normal">Normal</button></li>
+        <li><button data-size="large">Large</button></li>
+    </ul>
+</div>
+
 <a title="Add tool" class="addd" href="/99/add.html">Add tool</a>
 `);
