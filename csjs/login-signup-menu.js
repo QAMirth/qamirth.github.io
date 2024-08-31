1574681b98ef1d1ee14a8f50277ef4ba2345d6cb
QAMirth <katzeniy008@gmail.com>
2024-08-31 18:39:56 +0300

    Update login-signup-menu.js

--- csjs/login-signup-menu.js
+++ csjs/login-signup-menu.js
@@ -1,12 +1,10 @@
 document.write(`
-<div id="textSizeFilter" style="position: relative;">
-    <button id="eyeButton" style="font-size: 24px;">👁</button>
-    <ul id="sizeOptions" style="display: none; list-style-type: none; padding: 0; margin: 0; position: absolute; background-color: white; border: 1px solid #ccc;">
+ <button id="eyeButton">👁</button>
+    <ul id="sizeOptions" style="display: none;">
         <li><button data-size="small">Small</button></li>
         <li><button data-size="normal">Normal</button></li>
         <li><button data-size="large">Large</button></li>
     </ul>
-</div>
 
 <a title="Add tool" class="addd" href="/99/add.html">Add tool</a>
 `);
