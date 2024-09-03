ba322de4d88ed91665d35228b92644a01a8037eb
QAMirth <katzeniy008@gmail.com>
2024-09-04 02:42:22 +0300

    Update opec.js

--- csjs/opec.js
+++ csjs/opec.js
@@ -7,13 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
         // Инициализация
         buttonLink2.style.display = 'block';
         buttonLink3.style.display = 'none';
-        buttonLink2.textContent = 'ˇ';
+        buttonLink2.textContent = '+';
 
         // При нажатии на .button-link2
         buttonLink2.addEventListener('click', function() {
             if (!desc.classList.contains('expanded')) {
                 desc.classList.add('expanded'); // Разворачиваем текст
-                buttonLink2.textContent = '̌^';
+                buttonLink2.textContent = '-';
                 buttonLink3.style.display = 'inline';
                 buttonLink3.textContent = 'Close block';
             } else {
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         function collapseText() {
             desc.classList.remove('expanded'); // Сворачиваем текст
-            buttonLink2.textContent = 'ˇ';
+            buttonLink2.textContent = '+';
             buttonLink3.style.display = 'none';
         }
     });
