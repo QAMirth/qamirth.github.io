4d4920ef4fb5cdef91c3ea7cfdee3c8f74fe250b
QAMirth <katzeniy008@gmail.com>
2024-09-19 02:56:25 +0300

    Update footer.js

--- csjs/footer.js
+++ csjs/footer.js
@@ -1,18 +1,18 @@
  document.write(`
-      <div class="container footer-container">
-        <div class="footer-left">
-          <div class="copy">
-            © <span id="currentYear"></span> <span class="kz"><a href="../99/contacts.html">Katzeniy</a></span>  
-          </div>
-          <div class="copy2"></div>
-        </div>
-        <div class="footer-right">
-          <span class="katzeniy">
-            <a href="../99/contacts.html">Author</a>
-          </span>
-          <span class="adt2">
-            <a href="../99/add.html">Add a tool</a>
-          </span>
-        </div>
-      </div>
+<div class="container footer-container">
+  <div class="footer-top">
+    <div class="footer-up">
+      <span id="resolution-info"></span > <span >|</span> <span  id="browser-info"></span>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="footer-left">
+      <a class="footer-link" href="#" data-modal="feedbackModal">Feedback</a> <span >|</span> <a class="footer-link" href="#" data-modal="addModal">Add software to the site</a> <span >|</span> <a class="footer-link" href="#" data-modal="contactsModal">Contacts</a>
+    </div>
+    <div class="footer-right">
+      <a class="footer-link" href="#" data-modal="aboutModal">About</a> <span>|</span> <a href="../99/katzeniy.html">Katzeniy</a> <span >|</span> <a class="footer-link2" href="../99/katzeniy.html">Author</a> <span >|</span> © <span id="currentYear"></span>
+    </div>
+
+  </div>
+</div>
     `);
