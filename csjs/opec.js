0df1b3994b12119d306641b289c292141362af74
QAMirth <katzeniy008@gmail.com>
2024-09-25 12:17:53 +0300

    Update opec.js

--- csjs/opec.js
+++ csjs/opec.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.info-card').forEach(card => {
         const desc = card.querySelector('.info-card-desc');
+        const content = card.querySelector('.info-card-content');
         const buttonLink2 = card.querySelector('.button-link2');
         const buttonLink3 = card.querySelector('.button-link3');
 
@@ -13,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
         buttonLink2.addEventListener('click', function() {
             if (!desc.classList.contains('expanded')) {
                 desc.classList.add('expanded'); // Разворачиваем текст
+                content.classList.add('expanded'); // Разворачиваем контент
                 buttonLink2.innerHTML = '<svg   class="close1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="close11" fill-rule="evenodd" clip-rule="evenodd" d="M7 6h10a6 6 0 0 1 0 12H7A6 6 0 0 1 7 6Zm13 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/></svg>';
                 buttonLink3.style.display = 'inline';
                 buttonLink3.textContent = '';
@@ -26,6 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         function collapseText() {
             desc.classList.remove('expanded'); // Сворачиваем текст
+            content.classList.remove('expanded'); // Сворачиваем контент
             buttonLink2.innerHTML = '<svg class="open1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path class="open11" fill-rule="evenodd" clip-rule="evenodd" d="M7 6h10a6 6 0 0 1 0 12H7A6 6 0 0 1 7 6Zm3 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/></svg>';
             buttonLink3.style.display = 'none';
         }
@@ -40,10 +43,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
         const card = document.querySelector(decodedAnchor);
         if (card) {
             const desc = card.querySelector('.info-card-desc');
+            const content = card.querySelector('.info-card-content');
             const buttonLink2 = card.querySelector('.button-link2');
             const buttonLink3 = card.querySelector('.button-link3');
 
             desc.classList.add('expanded'); // Разворачиваем текст по умолчанию
+            content.classList.add('expanded'); // Разворачиваем контент по умолчанию
             buttonLink2.innerHTML = '<svg class="close1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="close11" fill-rule="evenodd" clip-rule="evenodd" d="M7 6h10a6 6 0 0 1 0 12H7A6 6 0 0 1 7 6Zm13 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/></svg>'; // KURY: Меняем иконку на close1
             buttonLink3.style.display = 'inline'; // Показываем кнопку "Show Less"
         }
@@ -88,7 +93,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const card = document.querySelector(`[id="${anchor.slice(1)}"]`); // Убираем символ #
     if (card) {
       const desc = card.querySelector('.info-card-desc');
+       const content = card.querySelector('.info-card-content'); // Добавляем выбор элемента .info-card-content
       desc.classList.add('expanded');
+      content.classList.add('expanded'); // Разворачиваем контент
     }
   }
 });
