93460d766978d99e19e015f54d99f8768f6931e0
QAMirth <katzeniy008@gmail.com>
2024-09-26 14:02:47 +0300

    Update footer.js

--- csjs/footer.js
+++ csjs/footer.js
@@ -2,15 +2,15 @@
 <div class="container footer-container">
   <div class="footer-top">
     <div class="footer-up">
-      <span id="resolution-info"></span > <span >|</span> <span  id="browser-info"></span>
+      <span id="resolution-info"></span > <span >·</span> <span  id="browser-info"></span>
     </div>
   </div>
   <div class="footer-bottom">
     <div class="footer-left">
-      <a class="footer-link" href="#" data-modal="feedbackModal">Feedback</a> <span >|</span> <a class="footer-link" href="#" data-modal="addModal">Add software to the site</a> <span >|</span> <a class="footer-link" href="#" data-modal="contactsModal">Contacts</a>
+      <a class="footer-link" href="#" data-modal="feedbackModal">Feedback</a> <span >·</span> <a class="footer-link" href="#" data-modal="addModal">Add software to the site</a> <span >·</span> <a class="footer-link" href="#" data-modal="contactsModal">Contacts</a>
     </div>
     <div class="footer-right">
-      <a class="footer-link" href="#" data-modal="aboutModal">About</a> <span>|</span> <a href="../99/katzeniy.html">Katzeniy</a> <span >|</span> <a class="footer-link2" href="../99/katzeniy.html">Author</a> <span >|</span> © <span id="currentYear-home"></span>
+      <a class="footer-link" href="#" data-modal="aboutModal">About</a> <span>·</span> <a href="../99/katzeniy.html">Katzeniy</a> <span >·</span> <a class="footer-link2" href="../99/katzeniy.html">Author</a> <span >·</span> © <span id="currentYear-home"></span>
     </div>
 
   </div>
