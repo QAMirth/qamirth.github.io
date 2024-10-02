8723b9616e17cc4f0838f6079938763ee59fc25e
QAMirth <katzeniy008@gmail.com>
2024-10-02 12:10:34 +0300

    Update filtr2-order.js

--- csjs/filtr2-order.js
+++ csjs/filtr2-order.js
@@ -1,7 +1,7 @@
 document.write(`
 <div id="filters-order">
     <select id="sort-filter" onchange="sortMaterials()">
-        <option value="title-asc">Titleа (A)</option>
+        <option value="title-asc">Title (A)</option>
         <option value="title-desc">Title (D)</option>
     </select>
 </div>
