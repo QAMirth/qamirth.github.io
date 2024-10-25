08672be1633927d81edd9ab9c8b6db9413943f31
QAMirth <katzeniy008@gmail.com>
2024-10-25 19:18:31 +0300

    Update opec.js

--- csjs/opec.js
+++ csjs/opec.js
@@ -152,10 +152,10 @@ function setCSS(size) {
               
        
             } else if (size === 'minimum') {
-                root.style.setProperty('--mp-top', 'var(--10)');
-                root.style.setProperty('--mp-left', 'var(--15)');
-                root.style.setProperty('--mp-bottom', 'var(--10)');
-                root.style.setProperty('--mp-right', 'var(--15)');
+                root.style.setProperty('--mp-top', 'var(--15)');
+                root.style.setProperty('--mp-left', 'var(--20)');
+                root.style.setProperty('--mp-bottom', 'var(--15)');
+                root.style.setProperty('--mp-right', 'var(--20)');
                 root.style.setProperty('--mp-border-radius', 'var(--0)');
                 root.style.setProperty('--mp-margin-bottom', '-1px');
                 root.style.setProperty('--icc-display', 'none');
