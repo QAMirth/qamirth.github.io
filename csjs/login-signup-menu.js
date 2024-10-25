2df22c770e155b72a09451774a5f305384d2e5fb
QAMirth <katzeniy008@gmail.com>
2024-10-25 03:17:07 +0300

    Update login-signup-menu.js

--- csjs/login-signup-menu.js
+++ csjs/login-signup-menu.js
@@ -1,6 +1,6 @@
 document.write(`
 <div class="atb">
-  <a title="Add tool" class="footer-link"  href="#" data-modal="addModal">Add tool</a>
+  <a title="Add tool" class="footer-link"  href="#" data-modal="addModal">+ Add tool</a>
 </div>
 
 `);
